42dd362390d9cc41edec1cefd1bd8aabf147e8d2
Juan Manuel <68016784+madpingui@users.noreply.github.com>
2022-04-21 22:45:46 -0500

    Initial commit

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] float rotationSpeed;
+
+    [SerializeField] Transform player;
+
+    Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        Vector2 dir = player.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Quaternion r = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, r, rotationSpeed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+         rb.AddRelativeForce(new Vector3(moveSpeed * Time.fixedDeltaTime, 0f, 0f));
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] float rotationSpeed;
+
+    bool gameover = false;
+
+    Rigidbody2D rb;
+
+    Camera cam;
+
+    public AudioClip audC;
+    public AudioSource audS;
+
+    [SerializeField] Text score;
+
+    float cpt = 0;
+    int scr = 0;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void SetScore()
+    {
+        cpt += Time.deltaTime;
+        if (cpt >= 0.5f)
+        {
+            cpt = 0f;
+            scr++;
+            score.text = scr.ToString("000");
+        }
+    }
+
+    private void Update()
+    {
+        if (!gameover)
+        {
+            SetScore();
+
+            transform.Rotate(Vector3.forward * (Input.GetAxis("Horizontal")*-rotationSpeed) * Time.deltaTime);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!gameover)
+        {
+            rb.AddRelativeForce(new Vector3(moveSpeed * Time.fixedDeltaTime, 0f, 0f));
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (!gameover)
+        {
+            cam.transform.position = new Vector3(transform.position.x, transform.position.y, cam.transform.position.z);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!gameover)
+        {
+            gameover = true;
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<PolygonCollider2D>().enabled = false;
+            GetComponentInChildren<ParticleSystem>().Play();
+            audS.PlayOneShot(audC);
+
+            Invoke("Restart", 2);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
